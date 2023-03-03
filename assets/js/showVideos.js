7ff81ec18bfa25d6d79b7b6ee4ec2509b66cf4a1
Jethro <jethrotiago@hotmail.com>
2023-03-03 15:25:10 -0300

    Added elements to the template strings

--- assets/js/showVideos.js
+++ assets/js/showVideos.js
@@ -2,23 +2,25 @@ import { apiConect } from "./apiConect.js";
 
 const list = document.querySelector("[data-list]");
 
-function buildCards() {
+function buildCards(title, views, url, image) {
     const video = document.createElement("li");
     video.className = "videos__item";
     video.innerHTML = `
-        <iframe width="100%" height="72%" src="https://www.youtube.com/embed/DnxNKANULuU" 
-        title="AMANTIKIR - CAMPOS DO JORDÃO - Beira da Aventura - Episódio 02" frameborder="0"
+        <iframe width="100%" height="72%" src="${url}" 
+        title="${title}" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen></iframe>
         <div class="video-description">
-            <img src="./assets/img/beiralogo.png" alt="Beira da aventura channel logo">
-            <h3>AMANTIKIR - CAMPOS DO JORDÃO - Beira da Aventura - Episódio 02</h3>
-            <p>316 visualizações</p>
+            <img src="${image}" alt="Beira da aventura channel logo">
+            <h3>${title}</h3>
+            <p>${views}</p>
         </div>
     `
     return video;
 };
 
 async function videoList() {
-    const list = await apiConect.videoList();
+    const apiList = await apiConect.videoList();
+    apiList.forEach(element => list.appendChild(
+        buildCards(element.title, element.views, element.url, element.image)))
 };
