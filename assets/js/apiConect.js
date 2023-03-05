c7db85873c3551eea93c7776d6d05ee04a0ec536
Jethro <jethrotiago@hotmail.com>
2023-03-04 21:51:28 -0300

    Created asynchronous function with POST method

--- assets/js/apiConect.js
+++ assets/js/apiConect.js
@@ -5,6 +5,21 @@ async function videoList() {
     return convertedConection;
 };
 
+async function createVideo(title, views, url, image) {
+    const conection = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title,
+            views: views,
+            url: url,
+            image: image
+        })
+    });
+}
+
 export const apiConect = {
     videoList
 };
