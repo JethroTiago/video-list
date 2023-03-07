17842b6ae9c1283db5a27a7a745eff5a5e9dac2e
Jethro <jethrotiago@hotmail.com>
2023-03-07 13:29:45 -0300

    Created styles for the form

--- assets/js/createVideo.js
+++ assets/js/createVideo.js
@@ -1,8 +1,11 @@
 const form = document.querySelector("[data-form]");
 
-function createVideo() {
+function createVideo(event) {
+    event.preventDefault();
     const image = document.querySelector("[data-image]").value;
     const url = document.querySelector("[data-url]").value;
     const title = document.querySelector("[data-title]").value;
     const views = Math.floor(Math.random() * 10).toString();
 }
+
+form.addEventListener("submit", event => createVideo(event));
