6e3ba92ed14690cca52e8f69f14611d8ec6458a5
Jethro <jethrotiago@hotmail.com>
2023-03-07 10:12:29 -0300

    Created firsts variables and function

---
+++ assets/js/createVideo.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("[data-form]");
+
+function createVideo() {
+    const image = document.querySelector("[data-image]").value;
+    const url = document.querySelector("[data-url]").value;
+    const title = document.querySelector("[data-title]").value;
+    const views = Math.floor(Math.random() * 10).toString();
+}
