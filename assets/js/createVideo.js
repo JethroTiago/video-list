bf00fe774b25ff4d1a8b37fac18e8fd3fb04e53f
Jethro <jethrotiago@hotmail.com>
2023-03-09 11:01:23 -0300

    Declared function as async and imported apiConect

--- assets/js/createVideo.js
+++ assets/js/createVideo.js
@@ -1,11 +1,14 @@
+import { apiConect } from "./apiConect.js";
 const form = document.querySelector("[data-form]");
 
-function createVideo(event) {
+async function createVideo(event) {
     event.preventDefault();
     const image = document.querySelector("[data-image]").value;
     const url = document.querySelector("[data-url]").value;
     const title = document.querySelector("[data-title]").value;
     const views = Math.floor(Math.random() * 10).toString();
+
+    await apiConect.createVideo(title, views, url, image)
 }
 
 form.addEventListener("submit", event => createVideo(event));
