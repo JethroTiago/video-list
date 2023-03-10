8dfef562ca34abefee9ae06afd8972e36f096679
Jethro <jethrotiago@hotmail.com>
2023-03-10 12:36:28 -0300

    Created searchVideos async function

--- assets/js/searchVideo.js
+++ assets/js/searchVideo.js
@@ -1,6 +1,13 @@
 import { apiConect } from "./apiConect.js";
 
-async function searchVideos() {
+async function searchVideos(e) {
+    e.preventDefault();
     const searchData = document.querySelector("[data-search]").value;
     const search = await apiConect.searchVideo(searchData);
+
+    console.log(search);
 }
+
+const searchButton = document.querySelector("[data-search-button]");
+
+searchButton.addEventListener("click", e => searchVideos(e))
