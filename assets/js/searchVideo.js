56c9ffad1debaaaea09ea31b6526d15a54c158a2
Jethro <jethrotiago@hotmail.com>
2023-03-10 12:07:50 -0300

    Added data attribute to the button

--- assets/js/searchVideo.js
+++ assets/js/searchVideo.js
@@ -1,6 +1,6 @@
 import { apiConect } from "./apiConect.js";
 
 async function searchVideos() {
-    const searchData = document.querySelector("[data-search]");
+    const searchData = document.querySelector("[data-search]").value;
     const search = await apiConect.searchVideo(searchData);
 }
