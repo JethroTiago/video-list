9759ea7fc8cd8ef1c53262b9f527557e04c244e2
Jethro <jethrotiago@hotmail.com>
2023-03-11 21:41:52 -0300

    Ending search engine

--- assets/js/searchVideo.js
+++ assets/js/searchVideo.js
@@ -1,12 +1,14 @@
 import { apiConect } from "./apiConect.js";
 import buildCards from "./showVideos.js";
 
-async function searchVideos(e) {
-    e.preventDefault();
+async function searchVideos(event) {
+    event.preventDefault();
     const searchData = document.querySelector("[data-search]").value;
     const search = await apiConect.searchVideo(searchData);
 
     const list = document.querySelector("[data-list]");
+    busca.forEach(element => list.appendChild(
+        buildCards(element.title, element.views, element.url, element.image)));
 }
 
 const searchButton = document.querySelector("[data-search-button]");
