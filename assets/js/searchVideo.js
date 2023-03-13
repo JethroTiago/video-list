0f052b99576f6d28434adcc5ac8095a3e24e98d0
Jethro <jethrotiago@hotmail.com>
2023-03-13 09:55:55 -0300

    Creating error message for video search

--- assets/js/searchVideo.js
+++ assets/js/searchVideo.js
@@ -14,6 +14,10 @@ async function searchVideos(event) {
 
     search.forEach(element => list.appendChild(
         buildCards(element.title, element.views, element.url, element.image)));
+
+        if (search.length == 0) {
+            list.innerHTML = `<h2 class="mensage__title">No video was found with these terms<h2/>`
+        }
 }
 
 const searchButton = document.querySelector("[data-search-button]");
