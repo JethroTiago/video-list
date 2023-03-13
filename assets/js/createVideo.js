502406fbe398c0062451200d5bc42ced5c57923f
Jethro <jethrotiago@hotmail.com>
2023-03-13 09:51:54 -0300

    Creating error message for video upload

--- assets/js/createVideo.js
+++ assets/js/createVideo.js
@@ -7,10 +7,13 @@ async function createVideo(event) {
     const url = document.querySelector("[data-url]").value;
     const title = document.querySelector("[data-title]").value;
     const views = Math.floor(Math.random() * 10).toString();
-
+try {
     await apiConect.createVideo(title, views, url, image)
 
     window.location.href = "./assets/pages/send-completed.html";
+    } catch (e) {
+        alert(e);
+    }
 }
 
 form.addEventListener("submit", event => createVideo(event));
